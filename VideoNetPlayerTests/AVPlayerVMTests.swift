68657b6dc4d8e6a74610868c71a69f44b721840d
Taras <minin@vsk.ru>
2019-02-10 23:45:47 +0700

    Asset Loading flag test

--- VideoNetPlayerTests/AVPlayerVMTests.swift
+++ VideoNetPlayerTests/AVPlayerVMTests.swift
@@ -40,6 +40,22 @@ class AVPlayerVMTests: XCTestCase {
     }
     
     func test_assetLoad() {
+        let isLoading = scheduler.createObserver(Bool.self)
         
+        sut.isLoadingAsset
+            .asDriver()
+            .drive(isLoading)
+            .disposed(by: disposeBag)
+        
+        let stubUrl = URL(string: "https://sample-videos.com/video123/mp4/480/big_buck_bunny_480p_30mb.mp4")!
+        scheduler.createColdObservable([.next(10, stubUrl)])
+            .bind(to: sut.videoUrl)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        
+    XCTAssertEqual(isLoading.events, [.next(0, false),
+                                      .next(10, true)])
     }
 }
